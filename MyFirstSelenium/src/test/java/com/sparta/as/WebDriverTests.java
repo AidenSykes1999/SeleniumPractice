285a0a6ebc80ca3a746578c9f52b69a22b7843b8
Aiden Sykes <aidensykes1999@gmail.com>
2022-02-28 12:04:03 +0000

    Changing Tabs Test

--- MyFirstSelenium/src/test/java/com/sparta/as/WebDriverTests.java
+++ MyFirstSelenium/src/test/java/com/sparta/as/WebDriverTests.java
@@ -2,6 +2,7 @@ package com.sparta.as;
 
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -40,6 +41,40 @@ public class WebDriverTests {
 
         Assertions.assertEquals(LocalDate.now().minusDays(1).toString(),pastDate);
     }
+
+    @Test
+    @DisplayName("Test that the search box works")
+    void testSearchBoxWorks(){
+        driver.findElement(By.name("q")).sendKeys("Java", Keys.ENTER);
+    }
+
+
+    @Test
+    @DisplayName("Managing settings for the webdriver")
+    void managingSettingsForTheWebdriver(){
+        driver.findElement(By.linkText("past")).click();
+        driver.navigate().forward();
+        driver.navigate().back();
+        System.out.println(driver.getCurrentUrl());
+    }
+
+    @Test
+    void changingTabs(){
+        String originalTab = driver.getWindowHandle();
+        System.out.println("First tab: " + driver.getWindowHandle());
+        driver.findElement(By.linkText("past")).sendKeys(Keys.chord(Keys.CONTROL,Keys.ENTER));
+        System.out.println("Second tab: " + driver.getWindowHandle());
+        System.out.println("All Tabs: " + driver.getWindowHandles());
+
+        for (String tab: driver.getWindowHandles()){
+            if(!originalTab.contains(tab)){
+                driver.switchTo().window(tab);
+                break;
+            }
+        }
+    }
+
+
 
         @AfterAll
     static void tearDownAll(){
