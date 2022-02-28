e109d0d2b39a1f90ddb90a35954597a17e1eacc4
Aiden Sykes <aidensykes1999@gmail.com>
2022-02-28 10:36:50 +0000

    First Selenium Test

--- MyFirstSelenium/src/test/java/com/sparta/as/WebDriverTests.java
+++ MyFirstSelenium/src/test/java/com/sparta/as/WebDriverTests.java
@@ -1,19 +1,35 @@
 package com.sparta.as;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverTests {
 
+    private static WebDriver driver;
+
+    @BeforeAll
+    static void setupAll(TestInfo testInfo){
+        System.out.println(testInfo.getDisplayName());
+        driver = new ChromeDriver();
+    }
+
     @Test
     @DisplayName("Checking the webdriver works")
     void checkingThatTheWebDriverWorks(){
-        WebDriver driver = new ChromeDriver();
+        driver.get("http://news.ycombinator.com/");
+        driver.findElement(By.linkText("past")).click();
 
-        driver.get("http://www.google.com");
+        Assertions.assertEquals("https://news.ycombinator.com/front",driver.getCurrentUrl());
+        }
 
+
+
+        @AfterAll
+    static void tearDownAll(){
+        //driver.close(); // close all open windows but not driver
+        //driver.quit(); // closes everything
         }
 
     }
