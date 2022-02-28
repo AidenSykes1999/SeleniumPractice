897762dc15bc21f6ae4d4d88388add899c1e2e6d
Aiden Sykes <aidensykes1999@gmail.com>
2022-02-28 23:18:39 +0000

    HackerTests

--- MyFirstSelenium/src/test/java/com/sparta/as/WebDriverTests.java
+++ MyFirstSelenium/src/test/java/com/sparta/as/WebDriverTests.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
 
 public class WebDriverTests {
 
@@ -21,20 +22,24 @@ public class WebDriverTests {
 
     @BeforeEach
     void setup(){
+
         driver.get("http://news.ycombinator.com/");
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
     }
 
 
     @BeforeAll
     static void setupAll(TestInfo testInfo){
-
+System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
         System.out.println(testInfo.getDisplayName());
         options = new ChromeOptions();
-        options.addArguments("headless");
+        //options.addArguments("headless");
 
         service = new ChromeDriverService
                 .Builder()
-                .usingDriverExecutable(new File("chromedriver.exe"))
+                .usingDriverExecutable(new File("src/test/resources/chromedriver.exe"))
                 .usingAnyFreePort()
                 .build();
 
@@ -44,7 +49,7 @@ public class WebDriverTests {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver(service, options);
 
     }
 
@@ -73,7 +78,10 @@ public class WebDriverTests {
 
 
     @Test
+    @DisplayName("Test that tabs can be created")
+
     void changingTabs(){
+
         String originalTab = driver.getWindowHandle();
         System.out.println("First tab: " + driver.getWindowHandle());
         driver.findElement(By.linkText("past")).sendKeys(Keys.chord(Keys.CONTROL,Keys.ENTER));
@@ -89,12 +97,49 @@ public class WebDriverTests {
     }
 
 
+    @Test
+    @DisplayName("Check if the newest page has 30 articles")
+    void checkIfNewestPageHas30Articles(){
+
+        driver.findElement(By.linkText("new")).click();
+        int count = driver.findElements(By.className("athing")).size();
+        Assertions.assertEquals(count,30);
 
 
+
+    }
+
+    @Test
+    @DisplayName("Check if the past page has 30 articles")
+    void checkIfPastPageHas30Articles(){
+
+        driver.findElement(By.linkText("past")).click();
+        int count = driver.findElements(By.className("athing")).size();
+        Assertions.assertEquals(count,30);
+
+
+
+    }
+
+@Test
+@DisplayName("Check if you need to be logged in to submit requests")
+void checkIfNeedsToBeLoggedIn(){
+
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.findElement(By.linkText("submit")).click();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        Boolean canLogin = driver.getPageSource().startsWith("You have to be logged in to submit.");
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        Assertions.assertTrue(canLogin);
+
+
+
+}
 
 
         @AfterAll
     static void tearDownAll(){
+        service.stop();
         //driver.close(); // close all open windows but not driver
         //driver.quit(); // closes everything
         }
